4c31b58a356deeb73f5e12fc1f8f03581a9a3376
TAN QIAO YUE <17045679@rp.edu.sg>
2019-05-30 12:59:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demoalarmmanager/AlarmReceiverActivity.java
@@ -0,0 +1,13 @@
+package com.example.demoalarmmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmReceiverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_receiver);
+    }
+}
---
+++ app/src/main/java/com/example/demoalarmmanager/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.demoalarmmanager;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSetAlarm;
+    AlarmManager am;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSetAlarm = this.findViewById(R.id.btnAlarm);
+
+        btnSetAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.SECOND, 5);
+
+                //Intent
+                Intent intent = new Intent(MainActivity.this, AlarmReceiverActivity.class);
+                int reqCode = 12345;
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,reqCode,intent,PendingIntent.FLAG_CANCEL_CURRENT);
+
+                //Get AlarmManager instance
+                am = (AlarmManager) getSystemService(Activity.ALARM_SERVICE);
+
+                //Set the alarm
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),pendingIntent);
+            }
+        });
+    }
+}
